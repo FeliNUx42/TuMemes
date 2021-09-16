cc4357184c3d047d85cff53456c5138873d8aba4
Felix Bommier <felix.bommier@gmail.com>
2021-09-16 18:46:52 +0200

    search functio

--- app/home.py
+++ app/home.py
@@ -16,6 +16,40 @@ home = Blueprint('home', __name__)
       del session["SEARCH_QUERY"]
 """
 
+def get_result():
+  name = request.form.get("name")
+  description = request.form.get("description")
+  #gender = request.form.get("gender")
+  memes = request.form.get("memes")
+  min_age = request.form.get("min-age")
+  max_age = request.form.get("max-age")
+  country = request.form.get("country")
+  city = request.form.get("city")
+
+  result = User.query
+
+  if name:
+    result = result.filter(or_(User.first_name.ilike(f"%{name}%"), User.last_name.ilike(f"%{name}%")))
+  if description:
+    result = result.filter(User.description.ilike(f"%{description}%"))
+  #if gender:
+  #  result = result.filter(User.gender.ilike(f"%{gender}%"))
+  if memes:
+    result = result.filter(User.meme_taste.ilike(f"%{memes}%"))
+  if min_age and min_age.isnumeric():
+    bday = datetime.now() - timedelta(days=365*(int(min_age)-1))
+    result = result.filter(User.birthday <= bday)
+  if max_age and max_age.isnumeric():
+    bday = datetime.now() - timedelta(days=365*(int(max_age)+1))
+    result = result.filter(User.birthday >= bday)
+  if country:
+    result = result.filter(User.country.ilike(f"%{country}%"))
+  if city:
+    result = result.filter(User.city.ilike(f"%{city}%"))
+    
+  return result
+
+
 @home.route('/', methods=["GET", "POST"])
 def index():
   search = request.args.get("search")
@@ -23,7 +57,7 @@ def index():
   per_page = request.args.get("per-page", current_app.config['RESULTS_PER_PAGE'], type=int)
 
   if not current_user.is_authenticated: current_user.username = ""
-  result = User.query.filter(User.username != current_user.username).order_by(User.created.desc()).paginate(page, per_page, True)
+  result = get_result().order_by(User.created.desc()).paginate(page, per_page, True)
 
   return render_template("main/home.html", result=result)
 
